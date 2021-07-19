655fdd3344462251c2595fb2b3d440573ec2fc59
unknown <neiyeattah@gmail.com>
2021-07-19 22:34:14 +0100

    cam

---
+++ camera/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace camera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.carCam = new Bunifu.Framework.UI.BunifuDropdown();
+            this.gunaCircleButton1 = new Guna.UI.WinForms.GunaCircleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(1, -2);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(468, 504);
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // carCam
+            // 
+            this.carCam.BackColor = System.Drawing.Color.Transparent;
+            this.carCam.BorderRadius = 3;
+            this.carCam.ForeColor = System.Drawing.Color.White;
+            this.carCam.Items = new string[0];
+            this.carCam.Location = new System.Drawing.Point(475, 28);
+            this.carCam.Name = "carCam";
+            this.carCam.NomalColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.carCam.onHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.carCam.selectedIndex = -1;
+            this.carCam.Size = new System.Drawing.Size(217, 35);
+            this.carCam.TabIndex = 3;
+            this.carCam.Tag = "";
+            // 
+            // gunaCircleButton1
+            // 
+            this.gunaCircleButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaCircleButton1.AnimationSpeed = 0.03F;
+            this.gunaCircleButton1.BaseColor = System.Drawing.Color.Gold;
+            this.gunaCircleButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaCircleButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaCircleButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaCircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaCircleButton1.ForeColor = System.Drawing.Color.White;
+            this.gunaCircleButton1.Image = null;
+            this.gunaCircleButton1.ImageSize = new System.Drawing.Size(52, 52);
+            this.gunaCircleButton1.Location = new System.Drawing.Point(543, 273);
+            this.gunaCircleButton1.Name = "gunaCircleButton1";
+            this.gunaCircleButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.gunaCircleButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaCircleButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaCircleButton1.OnHoverImage = null;
+            this.gunaCircleButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaCircleButton1.Size = new System.Drawing.Size(111, 97);
+            this.gunaCircleButton1.TabIndex = 6;
+            this.gunaCircleButton1.Text = "CAPTURE";
+            this.gunaCircleButton1.Click += new System.EventHandler(this.gunaCircleButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(713, 502);
+            this.Controls.Add(this.gunaCircleButton1);
+            this.Controls.Add(this.carCam);
+            this.Controls.Add(this.pic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private Bunifu.Framework.UI.BunifuDropdown carCam;
+        private Guna.UI.WinForms.GunaCircleButton gunaCircleButton1;
+    }
+}
+
---
+++ camera/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace camera
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection fil;
+        VideoCaptureDevice vid;
+        private void gunaCircleButton1_Click(object sender, EventArgs e)
+        {
+            vid = new VideoCaptureDevice(fil[carCam.selectedIndex].MonikerString);
+            vid.NewFrame += vid_NewFrame;
+            vid.Start();
+        }
+
+        void vid_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pic.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fil = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in fil)
+                carCam.AddItem(filterInfo.Name);
+            carCam.selectedIndex = 0;
+            vid = new VideoCaptureDevice();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           if (vid.IsRunning == true)
+                vid.Stop();
+        }
+    }
+}
